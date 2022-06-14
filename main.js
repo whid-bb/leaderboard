0bbf31f040a810a755f1cdd7c042f5a7847bb496
whid-bb <whid11s50@gmail.com>
2022-06-14 17:26:33 +0200

    add dist dir

---
+++ main.js
@@ -0,0 +1,66 @@
+"use strict";
+(self["webpackChunkproject_name"] = self["webpackChunkproject_name"] || []).push([["main"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _js_api_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./js/api.js */ "./src/js/api.js");
+
+var api = new _js_api_js__WEBPACK_IMPORTED_MODULE_0__["default"]();
+api.init();
+
+/***/ }),
+
+/***/ "./src/js/api.js":
+/*!***********************!*\
+  !*** ./src/js/api.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var LeaderboardApi = /*#__PURE__*/_createClass(function LeaderboardApi() {
+  _classCallCheck(this, LeaderboardApi);
+
+  _defineProperty(this, "init", function () {
+    return 0;
+  });
+});
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (LeaderboardApi);
+
+/***/ }),
+
+/***/ "./src/scss/base.scss":
+/*!****************************!*\
+  !*** ./src/scss/base.scss ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"), __webpack_exec__("./src/scss/base.scss"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7OztBQUFBO0FBRUEsSUFBTUMsR0FBRyxHQUFHLElBQUlELGtEQUFKLEVBQVo7QUFDQUMsR0FBRyxDQUFDQyxJQUFKOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7O0lDSE1GOzs7Z0NBQ0c7SUFBQSxPQUFNLENBQU47RUFBQTs7O0FBR1QsaUVBQWVBLGNBQWY7Ozs7Ozs7Ozs7O0FDSkEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9wcm9qZWN0IG5hbWUvLi9zcmMvaW5kZXguanMiLCJ3ZWJwYWNrOi8vcHJvamVjdCBuYW1lLy4vc3JjL2pzL2FwaS5qcyIsIndlYnBhY2s6Ly9wcm9qZWN0IG5hbWUvLi9zcmMvc2Nzcy9iYXNlLnNjc3M/OWMyNyJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgTGVhZGVyYm9hcmRBcGkgZnJvbSAnLi9qcy9hcGkuanMnO1xuXG5jb25zdCBhcGkgPSBuZXcgTGVhZGVyYm9hcmRBcGkoKTtcbmFwaS5pbml0KCk7IiwiY2xhc3MgTGVhZGVyYm9hcmRBcGkge1xuICBpbml0ID0gKCkgPT4gMDtcbn1cblxuZXhwb3J0IGRlZmF1bHQgTGVhZGVyYm9hcmRBcGk7IiwiLy8gZXh0cmFjdGVkIGJ5IG1pbmktY3NzLWV4dHJhY3QtcGx1Z2luXG5leHBvcnQge307Il0sIm5hbWVzIjpbIkxlYWRlcmJvYXJkQXBpIiwiYXBpIiwiaW5pdCJdLCJzb3VyY2VSb290IjoiIn0=
